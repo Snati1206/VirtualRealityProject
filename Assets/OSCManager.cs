50ce0b33526c7e2b03c5963069396ebb71696c79
Santiago Barragan <94990891+Snati1206@users.noreply.github.com>
2025-03-05 00:46:06 +0100

    Cube rotating with OSC messages

--- Assets/OSCManager.cs
+++ Assets/OSCManager.cs
@@ -6,6 +6,9 @@ public class OSCManager : MonoBehaviour
 {
     public string networkIP = "127.0.0.1";
     public int port = 8000;
+    public float sensor1;
+    public float sensor2;
+    public float sensor3;
 
     private OSCReceiver receiver;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,20 +17,45 @@ public class OSCManager : MonoBehaviour
         receiver = gameObject.AddComponent<OSCReceiver>();
         receiver.LocalHost = networkIP;
         receiver.LocalPort = port;
-        receiver.Bind("/sensor1", TestMethod);
-        receiver.Bind("/sensor2", TestMethod);
-        receiver.Bind("/sensor3", TestMethod);
+        receiver.Bind("/sensor1", OnReceiveSensor1);
+        receiver.Bind("/sensor2", OnReceiveSensor2);
+        receiver.Bind("/sensor3", OnReceiveSensor3);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (receiver == null) 
+        {
+            Debug.LogError("OSCReceiver not initialized.");
+            return;
+        }
+
         
     }
 
-    private void TestMethod(OSCMessage message)
+    private void OnReceiveSensor1(OSCMessage message)
     {
-        Debug.Log(message);
+        if (message.ToFloat(out float value))
+        {
+            sensor1 = value;
+        }
+    }
+
+    private void OnReceiveSensor2(OSCMessage message)
+    {
+        if (message.ToFloat(out float value))
+        {
+            sensor2 = value;
+        }
+    }
+
+    private void OnReceiveSensor3(OSCMessage message)
+    {
+        if (message.ToFloat(out float value))
+        {
+            sensor3 = value;
+        }
     }
 }
---
+++ Assets/PositionTransformer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PositionTransformer : MonoBehaviour
+{
+    public GameObject cube;
+    public OSCManager oscManager;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (cube == null)
+        {
+            Debug.LogError("No 3D object have been asigned !");
+            return;
+        }
+
+        if (oscManager == null)
+        {
+            Debug.LogError("No OSCManager have been asigned !");
+            return;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cube != null && oscManager != null)
+        {
+            cube.transform.rotation = Quaternion.Euler(oscManager.sensor1, oscManager.sensor2, oscManager.sensor3);
+        }
+        
+    }
+}
+
