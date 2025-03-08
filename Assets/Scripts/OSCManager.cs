fbf95b7fe119090006649c1a35a89f350a98e03c
Santiago Barragan <94990891+Snati1206@users.noreply.github.com>
2025-03-08 18:20:10 +0100

    Cleaning Project

---
+++ Assets/Scripts/OSCManager.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using extOSC;
+using System;
+
+public class OSCManager : MonoBehaviour
+
+{
+    public string networkIP = "127.0.0.1";
+    public int port = 8000;
+    public static float sensor1;
+    public static float sensor2;
+    public static float sensor3;
+    public bool oscOn = false;
+
+    private OSCReceiver receiver;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        receiver = gameObject.AddComponent<OSCReceiver>();
+        receiver.LocalHost = networkIP;
+        receiver.LocalPort = port;
+        receiver.Bind("/sensor1", OnReceiveSensor1);
+        receiver.Bind("/sensor2", OnReceiveSensor2);
+        receiver.Bind("/sensor3", OnReceiveSensor3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+    }
+
+    private void OnReceiveSensor1(OSCMessage message)
+    {
+        if (message.ToFloat(out float value))
+        {
+            sensor1 = value;
+        }
+        else
+        {
+            sensor1 = 100f;
+            Debug.LogError("OSC message out borders.");
+        }
+    }
+
+    private void OnReceiveSensor2(OSCMessage message)
+    {
+        if (message.ToFloat(out float value))
+        {
+            sensor2 = value;
+        }
+         else
+        {
+            sensor2 = 100f;
+            Debug.LogError("OSC message out borders.");
+        }
+    }
+
+    private void OnReceiveSensor3(OSCMessage message)
+    {
+        if (message.ToFloat(out float value))
+        {
+            sensor3 = value;
+        }
+        else
+        {
+            sensor3 = 100f;
+            Debug.LogError("OSC message out borders.");
+        }
+
+    }
+}
