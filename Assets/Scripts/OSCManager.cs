d40417e5c49965d9de9ba514152d79b4b6c8c902
Santiago Barragan <94990891+Snati1206@users.noreply.github.com>
2025-03-11 18:48:54 +0100

    Music

--- Assets/Scripts/OSCManager.cs
+++ Assets/Scripts/OSCManager.cs
@@ -5,11 +5,11 @@ using System;
 public class OSCManager : MonoBehaviour
 
 {
-    public string networkIP = "127.0.0.1";
-    public int port = 8000;
-    public static float sensor1;
-    public static float sensor2;
-    public static float sensor3;
+    public string networkIP = "192.168.0.164";
+    public int port = 9999;
+    [SerializeField] public static float sensor1;
+    [SerializeField] public static float sensor2;
+    [SerializeField] public static float sensor3;
     public bool oscOn = false;
 
     private OSCReceiver receiver;
@@ -19,14 +19,16 @@ public class OSCManager : MonoBehaviour
         receiver = gameObject.AddComponent<OSCReceiver>();
         receiver.LocalHost = networkIP;
         receiver.LocalPort = port;
-        receiver.Bind("/sensor1", OnReceiveSensor1);
-        receiver.Bind("/sensor2", OnReceiveSensor2);
-        receiver.Bind("/sensor3", OnReceiveSensor3);
+        receiver.Bind("/distance/1", OnReceiveSensor1);
+        receiver.Bind("/distance/2", OnReceiveSensor2);
+        receiver.Bind("/distance/3", OnReceiveSensor3);
+        Debug.Log("OSC Receiver initialized with IP: " + networkIP + " and Port: " + port);
     }
 
     // Update is called once per frame
     void Update()
-    {        
+    {     
+   
     }
 
     private void OnReceiveSensor1(OSCMessage message)
@@ -57,9 +59,11 @@ public class OSCManager : MonoBehaviour
 
     private void OnReceiveSensor3(OSCMessage message)
     {
+
         if (message.ToFloat(out float value))
         {
             sensor3 = value;
+
         }
         else
         {
